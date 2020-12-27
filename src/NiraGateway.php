1269e8fe3cd25d7785698626838bc90057b4be16
Mohammad Reza mansouri <beekalam@gmail.com>
2020-12-27 14:19:37 +0330

    [CODE] remvoed clientbuilder.

--- src/NiraGateway.php
+++ src/NiraGateway.php
@@ -2,22 +2,49 @@
 
 namespace Beekalam\NiraGateway;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+
 class NiraGateway
 {
+
+    private $user;
+    private $pass;
+    private $testing = false;
+    private $mock = null;
 
     /**
      * @var ClientBuilder
      */
     private ClientBuilder $clientBuilder;
 
-    public function __construct(ClientBuilder $client)
+    public function __construct($user, $pass)
     {
-        $this->clientBuilder = $client;
+        // $this->clientBuilder = $client;
+        $this->user = $user;
+        $this->pass = $pass;
+    }
+
+    public function getClient()
+    {
+        if ($this->testing == true) {
+            return $this->getTestingClient();
+        }
+        return $client = new Client([
+            'base_uri' => Constants::AVAILABILITY_URI,
+            'timeout'  => Constants::GATEWAY_TIMEOUT
+        ]);
+    }
+
+    public function getTestingClient(): Client
+    {
+        $handlerStack = HandlerStack::create($this->mock);
+        $client = new Client(['handler' => $handlerStack]);
+        return $client;
     }
 
     public function search($options)
     {
-
         $response = $this->clientBuilder
             ->getClient()
             ->request('GET', $this->clientBuilder->buildURL($options));
@@ -25,4 +52,43 @@ class NiraGateway
         return $response->getBody();
     }
 
+    /**
+     * @return mixed
+     */
+    public function getTesting()
+    {
+        return $this->testing;
+    }
+
+    /**
+     * @param bool $testing
+     * @return ClientBuilder
+     */
+    public function setTesting(bool $testing): ClientBuilder
+    {
+        $this->testing = $testing;
+        return $this;
+    }
+
+
+    /**
+     * @param null $mock
+     * @return ClientBuilder
+     */
+    public function setMock($mock)
+    {
+        $this->mock = $mock;
+        return $this;
+    }
+
+    public function buildURL(array $queryParams)
+    {
+        $query = http_build_query(
+            array_merge($queryParams, [
+                'officeUser' => $this->user,
+                'officePass' => $this->pass
+            ])
+        );
+        return Constants::AVAILABILITY_URI . '?' . $query;
+    }
 }
