82966affc63bd79723c51ed7fa68bd6d543a87b0
Mohammad Reza Mansouri <beekalam@users.noreply.github.com>
2021-02-23 08:06:14 +0000

    Apply fixes from StyleCI

--- tests/NiraGatewayTest.php
+++ tests/NiraGatewayTest.php
@@ -20,7 +20,7 @@ class NiraGatewayTest extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->niraGatewaySpecification = new NiraGatewaySpecification('http://some.domain/ws','user','pass');
+        $this->niraGatewaySpecification = new NiraGatewaySpecification('http://some.domain/ws', 'user', 'pass');
     }
 
     /** @test */
@@ -101,5 +101,4 @@ class NiraGatewayTest extends BaseTestCase
         $res = $ng->reserve($rp);
         $this->assertISJson($res);
     }
-
 }
