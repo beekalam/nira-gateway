a6e6f4729bf693c056fc42931c5e629ed8c21fc7
Mohammad Reza mansouri <beekalam@gmail.com>
2021-02-24 17:49:35 +0330

    [CODE,TEST] Reserve Ticket Parser.

---
+++ src/ReserveTicketParser.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Beekalam\NiraGateway;
+
+class ReserveTicketParser
+{
+    public $data;
+
+    private $totalPrice;
+
+    private $contact;
+
+    private $segments;
+
+    private $childTP;
+
+    private $infantTP;
+
+    private $adultTP;
+
+    private $tickets;
+
+    private $docs;
+
+    private $adultQTY;
+
+    private $childQTY;
+
+    private $infantQTY;
+
+    private $status;
+
+    private $office;
+
+    /**
+     * ReserveTicketParser constructor.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+
+        $this->adultQTY = $data['AdultQTY'];
+        $this->childQTY = $data['ChildQTY'];
+        $this->infantQTY = $data['InfantQTY'];
+
+        $this->totalPrice = $data['TotalPrice'];
+        $this->infantTP = $data['InfantTP'];
+        $this->adultTP = $data['AdultTP'];
+        $this->childTP = $data['ChildTP'];
+
+        $this->contact = $data['Contact'];
+        if (isset($data['Segments'])) {
+            $this->segments = $data['Segments'];
+        }
+
+        $this->tickets = $data['Tickets'];
+        $this->docs = $data['DOCS'];
+
+        $this->status = $data['Status'];
+        $this->office = $data['Office'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSegments()
+    {
+        return $this->segments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChildTP()
+    {
+        return $this->childTP;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInfantTP()
+    {
+        return $this->infantTP;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdultTP()
+    {
+        return $this->adultTP;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTickets()
+    {
+        return $this->tickets;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDocs()
+    {
+        return $this->docs;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdultQTY()
+    {
+        return $this->adultQTY;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChildQTY()
+    {
+        return $this->childQTY;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInfantQTY()
+    {
+        return $this->infantQTY;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOffice()
+    {
+        return $this->office;
+    }
+}
---
+++ tests/ReserveTicketParserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Beekalam\NiraGateway\Tests;
+
+use Beekalam\NiraGateway\ReserveTicketParser;
+
+class ReserveTicketParserTest extends BaseTestCase
+{
+    private $rt;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->rt = json_decode($this->getReserveInfoResults(), true);
+    }
+
+    /** @test */
+    function it_should_return_correct_Quantities_based_on_reserve_endpoint_data()
+    {
+        $rtp = new ReserveTicketParser($this->rt);
+
+        $this->assertEquals("1", $rtp->getAdultQTY());
+        $this->assertEquals("0", $rtp->getInfantQTY());
+        $this->assertEquals("0", $rtp->getChildQTY());
+    }
+
+    /** @test */
+    function it_should_return_correct_prices()
+    {
+        $rtp = new ReserveTicketParser($this->rt);
+
+        $this->assertEquals("2280000", $rtp->getAdultTP());
+        $this->assertEquals("1220000", $rtp->getChildTP());
+        $this->assertEquals("372000", $rtp->getInfantTP());
+        $this->assertEquals("2280000", $rtp->getTotalPrice());
+    }
+
+    /** @test */
+    function it_should_return_correct_segments()
+    {
+        $rtp = new ReserveTicketParser($this->rt);
+        $this->assertEquals($this->rt['Segments'], $rtp->getSegments());
+    }
+
+    /** @test */
+    function it_should_return_correct_tickets()
+    {
+        $rtp = new ReserveTicketParser($this->rt);
+        $this->assertEquals($this->rt['Tickets'], $rtp->getTickets());
+    }
+
+    /** @test */
+    function it_should_return_correct_docs()
+    {
+        $rtp = new ReserveTicketParser($this->rt);
+        $this->assertEquals($this->rt['DOCS'], $rtp->getDocs());
+    }
+
+    /** @test */
+    function it_should_return_correct_contact()
+    {
+        $rtp = new ReserveTicketParser($this->rt);
+        $this->assertEquals($this->rt['Contact'], $rtp->getContact());
+    }
+}
