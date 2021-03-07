c07003e9309f4fc16d2b1fd740441b5d93fdac28
Mohammad Reza Mansouri <beekalam@users.noreply.github.com>
2021-03-07 09:25:42 +0000

    Apply fixes from StyleCI

--- src/NiraGatewayException.php
+++ src/NiraGatewayException.php
@@ -7,7 +7,7 @@ class NiraGatewayException extends \Exception
     /**
      * When there's an internal server error on Nira API side
      * It spits out the username and password.
-     * This will be the \GuzzleHttp\RequestException that is throw
+     * This will be the \GuzzleHttp\RequestException that is throw.
      * @var
      */
     private $gatewayError;
